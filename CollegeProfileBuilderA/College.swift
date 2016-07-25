023da54f1d5c518cc493581a1f2a13ec1e98f26f
Saurav Desai <esowmya@sauravs-macbook-pro-2.local>
2016-07-25 16:55:15 -0500

    connected storyboard

---
+++ CollegeProfileBuilderA/College.swift
@@ -0,0 +1,17 @@
+//
+//  College.swift
+//  CollegeProfileBuilderA
+//
+//  Created by Saurav Desai on 7/25/16.
+//  Copyright © 2016 Saurav Desai. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+    var name = ""
+    var location = ""
+    var enrollment = 0
+    var image = UIImage(named: "genericCollege")
+
+}
--- CollegeProfileBuilderA/ViewController.swift
+++ CollegeProfileBuilderA/ViewController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var colleges : [College] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
