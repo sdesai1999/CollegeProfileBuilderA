6a55c7f9e9c48a8403d63f7f9750ee98baf5dc97
Saurav Desai <esowmya@sauravs-macbook-pro-2.local>
2016-07-25 19:56:30 -0500

    added segue to DetailViewController

--- CollegeProfileBuilderA/DetailViewController.swift
+++ CollegeProfileBuilderA/DetailViewController.swift
@@ -14,29 +14,19 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var enrollmentTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var savedLabel: UILabel!
     var college : College!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collegeTextField.delegate = self
-        locationTextField.delegate = self
-        enrollmentTextField.delegate = self
         collegeTextField.text = college.name
         locationTextField.text = college.location
         enrollmentTextField.text = "\(college.enrollment)"
         imageView.image = college.image
-        savedLabel.text = ""
     }
     
-    func textFieldDidBeginEditing(textField: UITextField) {
-        savedLabel.text = ""
-    }
-
     @IBAction func onTappedSaveButton(sender: AnyObject) {
         college.name = collegeTextField.text!
         college.location = locationTextField.text!
         college.enrollment = Int(enrollmentTextField.text!)!
-        savedLabel.text = "Saved"
     }
 }
--- CollegeProfileBuilderA/ViewController.swift
+++ CollegeProfileBuilderA/ViewController.swift
@@ -74,6 +74,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             sender.tag = 0
         }
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let dvc = segue.destinationViewController as! DetailViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.college = colleges[index!]
+    }
 }
 
 
